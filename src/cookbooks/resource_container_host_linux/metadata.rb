461a40956455706faa7b19233bec9189000513ee
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-04-30 21:48:56 +1200

    Updating the metadata for the cookbook to fix Foodcritic errors
    references #2

--- src/cookbooks/resource_container_host_linux/metadata.rb
+++ src/cookbooks/resource_container_host_linux/metadata.rb
@@ -1,13 +1,17 @@
+chef_version '>= 12.5' if respond_to?(:chef_version)
+description 'Environment cookbook that configures a Linux server as a container host with consul and nomad'
+issues_url '${ProductUrl}/issues' if respond_to?(:issues_url)
+license 'Apache-2.0'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name 'resource_container_host_linux'
 maintainer '${CompanyName} (${CompanyUrl})'
 maintainer_email '${EmailDocumentation}'
-license 'Apache v2.0'
-description 'Environment cookbook that configures a Linux server as a container host with consul and nomad'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+source_url '${ProductUrl}' if respond_to?(:source_url)
+version '${VersionSemantic}'
 
 supports 'ubuntu', '>= 16.04'
 
-depends 'consul', '>= 2.3.0'
-depends 'docker', '>= 2.15.5'
-depends 'nomad', '>= 0.12.0'
+depends 'consul', '= 2.3.0'
+depends 'consul_template', '= 0.1.0'
+depends 'docker', '= 2.15.5'
+depends 'nomad', '= 0.12.0'
