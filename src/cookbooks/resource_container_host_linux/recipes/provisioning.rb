08eb20ab950d50ce31393828298d4fc3569a4495
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-09-16 23:49:34 +1200

    Updating the provisioning to set a proper host name and to restart the machine after provisioning to ensure that the settings are correctly set

--- src/cookbooks/resource_container_host_linux/recipes/provisioning.rb
+++ src/cookbooks/resource_container_host_linux/recipes/provisioning.rb
@@ -8,6 +8,22 @@
 #
 
 #
+# INSTALL DOS2UNIX
+#
+
+apt_package 'dos2unix' do
+  action :install
+end
+
+#
+# INSTALL PWGEN
+#
+
+apt_package 'pwgen' do
+  action :install
+end
+
+#
 # CONFIGURE THE PROVISIONING SCRIPT
 #
 
@@ -26,56 +42,90 @@ file '/etc/init.d/provision.sh' do
         done< <(LANG=C /sbin/ifconfig eth0)
     }
 
+    function setHostName {
+      # Generate a 16 character password
+      POSTFIX=$(pwgen --no-capitalize 16 1)
+
+      NAME="cvcontainerhost-${RESOURCE_VERSION_MAJOR}-${RESOURCE_VERSION_MINOR}-${RESOURCE_VERSION_PATCH}-${POSTFIX}"
+      sudo hostnamectl set-hostname $NAME
+    }
+
     FLAG="/var/log/firstboot.log"
     if [ ! -f $FLAG ]; then
 
+      setHostName
+
       IPADDRESS=$(getEth0Ip)
 
-      # Create '/etc/consul/conf.d/client_connections.json'
+      #
+      # CREATE MACHINE SPECIFIC CONFIGURATION FILES
+      #
+      # Create '/etc/consul/conf.d/connections.json'
       echo "{ \\"advertise_addr\\": \\"${IPADDRESS}\\", \\"bind_addr\\": \\"${IPADDRESS}\\" }"  > /etc/consul/conf.d/connections.json
 
-      # Create '/etc/nomad-conf.d/client_connections.hcl'
+      # Create '/etc/nomad-conf.d/connections.hcl'
       echo -e "bind_addr = \\"${IPADDRESS}\\"\\n advertise {\\n  http = \\"${IPADDRESS}\\"\\n  rpc = \\"${IPADDRESS}\\"\\n  serf = \\"${IPADDRESS}\\"\\n}"  > /etc/nomad-conf.d/connections.hcl
 
+      #
+      # MOUNT THE DVD WITH THE CONFIGURATION FILES
+      #
       if [ ! -d /mnt/dvd ]; then
         mkdir /mnt/dvd
       fi
       mount /dev/dvd /mnt/dvd
 
+      #
+      # CONFIGURE SSH
+      #
       # If the allow SSH file is not there, disable SSH in the firewall
       if [ ! -f /mnt/dvd/allow_ssh.json ]; then
         ufw deny 22
       fi
 
+      #
+      # CONSUL CONFIGURATION
+      #
       cp -a /mnt/dvd/consul/consul_region.json /etc/consul/conf.d/region.json
+      dos2unix /etc/consul/conf.d/region.json
+
       cp -a /mnt/dvd/consul/consul_secrets.json /etc/consul/conf.d/secrets.json
+      dos2unix /etc/consul/conf.d/secrets.json
+
       cp -a /mnt/dvd/consul/client/consul_client_location.json /etc/consul/conf.d/location.json
+      dos2unix /etc/consul/conf.d/location.json
 
+      #
+      # NOMAD CONFIGURATION
+      #
       cp -a /mnt/dvd/nomad/nomad_region.hcl /etc/nomad-conf.d/region.hcl
+      dos2unix /etc/nomad-conf.d/region.hcl
+
       cp -a /mnt/dvd/nomad/nomad_secrets.hcl /etc/nomad-conf.d/secrets.hcl
+      dos2unix /etc/nomad-conf.d/secrets.hcl
 
-      # Copy the script that will be used to create the Docker IPVLAN network
-      cp -a /mnt/dvd/docker_ipvlan.sh /tmp/docker_ipvlan.sh
+      #
+      # UNBOUND CONFIGURATION
+      #
+      cp -a /mnt/dvd/unbound/unbound_zones.conf /etc/unbound.d/unbound_zones.conf
+      dos2unix /etc/unbound.d/unbound_zones.conf
 
-      # Copy the unbound configuration
-      cp -a /mnt/dvd/unbound/. /etc/unbound.d/
-
+      #
+      # UNMOUNT DVD
+      #
       umount /dev/dvd
+      eject -T /dev/dvd
 
-      # Run the script that creates the Docker IPVLAN network connection
-      chmod a+x /tmp/docker_ipvlan.sh
-      source /tmp/docker_ipvlan.sh
-
+      #
+      # ENABLE SERVICES
+      #
       sudo systemctl enable unbound.service
-      sudo systemctl restart unbound.service
-
-      sudo systemctl restart consul.service
-
       sudo systemctl enable nomad.service
-      sudo systemctl restart nomad.service
 
       # The next line creates an empty file so it won't run the next boot
       touch $FLAG
+
+      # restart the machine so that all configuration settings take hold
+      sudo shutdown -r now
     else
       echo "Provisioning script ran previously so nothing to do"
     fi
@@ -96,6 +146,7 @@ file '/etc/systemd/system/provision.service' do
     Type=oneshot
     ExecStart=/etc/init.d/provision.sh
     RemainAfterExit=true
+    EnvironmentFile=-/etc/environment
 
     [Install]
     WantedBy=network-online.target
