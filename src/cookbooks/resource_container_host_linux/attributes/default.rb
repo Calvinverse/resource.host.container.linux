b103b8c731332f96de7716a3ee4728d598671a05
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-03-09 22:02:49 +1300

    Updating the nomad recipe
    references #17

--- src/cookbooks/resource_container_host_linux/attributes/default.rb
+++ src/cookbooks/resource_container_host_linux/attributes/default.rb
@@ -32,5 +32,15 @@ default['firewall']['ipv6_enabled'] = false
 default['nomad']['package'] = '0.7.1/nomad_0.7.1_linux_amd64.zip'
 default['nomad']['checksum'] = '72b32799c2128ed9d2bb6cbf00c7600644a8d06c521a320e42d5493a5d8a789a'
 
+default['nomad']['service_user'] = 'nomad'
+default['nomad']['service_group'] = 'nomad'
 
+default['nomad']['consul_template_metrics_file'] = 'nomad_metrics.ctmpl'
+default['nomad']['consul_template_region_file'] = 'nomad_region.ctmpl'
+default['nomad']['consul_template_secrets_file'] = 'nomad_secrets.ctmpl'
+default['nomad']['consul_template_server_file'] = 'nomad_server.ctmpl'
 
+default['nomad']['metrics_file'] = 'metrics.hcl'
+default['nomad']['region_file'] = 'region.hcl'
+default['nomad']['secrets_file'] = 'secrets.hcl'
+default['nomad']['server_file'] = 'server.hcl'
