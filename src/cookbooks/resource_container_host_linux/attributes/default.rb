3b802ea3c33901ee93ed85794ddfa8c9bbb71fd1
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-03-09 22:00:08 +1300

    Removing the recipes that we won't need anymore and their attributes
    references #17

--- src/cookbooks/resource_container_host_linux/attributes/default.rb
+++ src/cookbooks/resource_container_host_linux/attributes/default.rb
@@ -1,50 +1,10 @@
 # frozen_string_literal: true
 
 #
-# CONSUL
 #
 
-default['consul']['version'] = '0.9.2'
-default['consul']['config']['domain'] = 'consulverse'
 
-# This is not a consul server node
-default['consul']['config']['server'] = false
 
-# For the time being don't verify incoming and outgoing TLS signatures
-default['consul']['config']['verify_incoming'] = false
-default['consul']['config']['verify_outgoing'] = false
-
-# Bind the client address to the local host. The advertise and bind addresses
-# will be set in a separate configuration file
-default['consul']['config']['client_addr'] = '127.0.0.1'
-
-# Do not allow consul to use the host information for the node id
-default['consul']['config']['disable_host_node_id'] = true
-
-# Disable remote exec
-default['consul']['config']['disable_remote_exec'] = true
-
-# Disable the update check
-default['consul']['config']['disable_update_check'] = true
-
-# Set the DNS configuration
-default['consul']['config']['dns_config'] = {
-  allow_stale: true,
-  max_stale: '87600h',
-  node_ttl: '10s',
-  service_ttl: {
-    '*': '10s'
-  }
-}
-
-# Always leave the cluster if we are terminated
-default['consul']['config']['leave_on_terminate'] = true
-
-# Send all logs to syslog
-default['consul']['config']['log_level'] = 'INFO'
-default['consul']['config']['enable_syslog'] = true
-
-default['consul']['config']['owner'] = 'root'
 
 #
 # FIREWALL
@@ -69,9 +29,5 @@ default['firewall']['ipv6_enabled'] = false
 default['nomad']['package'] = '0.7.1/nomad_0.7.1_linux_amd64.zip'
 default['nomad']['checksum'] = '72b32799c2128ed9d2bb6cbf00c7600644a8d06c521a320e42d5493a5d8a789a'
 
-default['unbound']['service_user'] = 'unbound'
-default['unbound']['service_group'] = 'unbound'
 
-default['paths']['unbound_config'] = '/etc/unbound.d'
 
-default['file_name']['unbound_config_file'] = 'unbound.conf'
